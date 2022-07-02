ebedcd4d4db3aa66b2c0a711c9b16949577b2bf4
Thomas Grbic <tomgrbz@gmail.com>
2022-07-02 17:03:28 -0400

    git logo and card height fixed

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -16,7 +16,7 @@ export default class Card extends React.Component<ICardProps, any> {
     }
     render() {
         return (
-            <div className="card card-compact w-96 max-height-100 bg-[#f6fad1] shadow-xl">
+            <div className="card card-compact w-96 h-96 bg-[#f6fad1] shadow-xl">
                 <figure><img src={require(`../images/${this.props.img}.png`)} alt={this.props.alt}/></figure>
                 <div className="card-body">
                     <h2 className="card-title">{this.props.title}</h2>
