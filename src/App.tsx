f8adc43867f2277302cc03f9b63905ded434c878
Thomas Grbic <tomgrbz@gmail.com>
2022-07-02 15:49:14 -0400

    every init

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import Layout from './components/layout'
 import './App.css';
+import { BrowserRouter, Route} from 'react-router-dom'
+
 
 function App() {
   return (
-    <Layout>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <Route  path='/'  element= {<Layout/>} />
 
-    </Layout>
+
+
+      </BrowserRouter>
   );
 }
 
