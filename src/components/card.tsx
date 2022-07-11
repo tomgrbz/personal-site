b464522bcaf77f2af24095cd1ddc3f4cb56adc53
tomgrbz <tomgrbz@gmail.com>
2022-07-11 15:23:54 -0400

    more mobile friendly cards and also added new tab links

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -14,14 +14,14 @@ const Card = (props:ICardProps) =>  {
     const { title, body, img, alt, route } = props;
 
         return (
-            <div className="card card-compact w-44 h-96 sm:w-96 sm:h-[360px] bg-[#f6fad1] shadow-xl">
-                <figure><img className="object-scale-down w-32 h-32 sm:w-52" src={require(`../images/${img}.png`)} alt={alt}/></figure>
+            <div className="card card-compact w-[fit] h-[fit] my-4 sm:w-96 sm:h-[360px] bg-[#f6fad1] shadow-xl">
+                <figure><img className="object-scale-down w-[fit] h-32 sm:w-[fit]" src={require(`../images/${img}.png`)} alt={alt}/></figure>
                 <div className="card-body">
-                    <h2 className="card-title">{title}</h2>
-                    <p className="text-sm">{body}</p>
+                    <h2 className="card-title font-black text-[#000000]">{title}</h2>
+                    <p className="text-sm font-black text-[#000000]">{body}</p>
                     <div className="card-actions justify-end">
-                        <a href={`https://github.com/tomgrbz/${route}`}>
-                        <button className="btn btn-accent">View on Github</button>
+                        <a href={`https://github.com/tomgrbz/${route}`} target="_blank" rel="noopener noreferrer">
+                        <button className="btn btn-accent w-24 sm:w-full">View on Github</button>
                         </a>
                     </div>
                 </div>
--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -3,7 +3,7 @@ import Card from './card'
 import Header from './header'
 import './daisy.css'
 
-import {Parallax, ParallaxLayer} from '@react-spring/parallax';
+
 
 
 export default class Layout extends React.Component<any, any> {
@@ -14,22 +14,22 @@ export default class Layout extends React.Component<any, any> {
 
     render() {
         return (
-                <div className="min-w-screen">
+                <div className="w-screen">
                     <div className="topContainer">
                     <Header/>
                     </div>
 
 
-                        <div className="h-[300] w-[fit] flex flex-wrap sm:flex sm:w-full sm:pt-10 sm:h-500px justify-between bg-[#96b78a]">
-                            <div className="flex px-16">
+                        <div className="h-[fit] w-[fit] flex flex-wrap sm:flex sm:w-full sm:pt-10 sm:h-500px justify-between bg-[#96b78a]">
+                            <div className="px-12">
                                 <Card title="TreeDB Website" body="A Website that contains data on trees in the local Boston area."
                                     img='treedb_site' alt="treedb_site" route="treedb"></Card>
                             </div>
-                            <div className="flex px-16">
+                            <div className="px-12">
                                 <Card title="Twilio Location Messenger" body="An app in progress that allows you to message people in a 1 mile radius."
                                       img='1024px-Twilio-logo-red.svg' alt="twilio_logo" route=""></Card>
                             </div>
-                            <div className="flex px-16">
+                            <div className="px-12">
                                 <Card title="AppleJacks" body="A discord bot that allows the user to play blackjack in discord through text commands"
                                       img='discord_bot' alt="discord_bot_img" route="BlackJackBot"></Card>
                             </div>
@@ -38,7 +38,7 @@ export default class Layout extends React.Component<any, any> {
 
                                 </div>
                                 <div className="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
-                                    <a href="https://github.com/tomgrbz" className="icon">
+                                    <a href="https://github.com/tomgrbz" className="icon" target="_blank" rel="noopener noreferrer">
                                         <img src={require("../images/GitHub-Mark-64px.png")} alt="github"></img>
                                     </a>
 
